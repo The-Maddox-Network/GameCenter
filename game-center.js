a5d94d14839408978ecc3c05c5e58673ce7956af
Maddox Nichols <maddox0816@gmail.com>
2021-07-01 20:01:39 -0400

    Add files via upload and create beta branch
    Upload files for redesigned game center and create beta branch.

---
+++ game-center.js
@@ -0,0 +1,20 @@
+$.fn.randomize = function(selector){
+    (selector ? this.find(selector) : this).parent().each(function(){
+        $(this).children(selector).sort(function(){
+            return Math.random() - 0.5;
+        }).detach().appendTo(this);
+    });
+
+    return this;
+};
+
+$('.game_center_page_wrap').randomize('.gc__img__wrap');
+
+window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
